7b042e54e1e02e99dfd510dc5d4de773f567e457
mohand3del <101574221+mohand3del@users.noreply.github.com>
2024-01-13 20:38:03 +0200

    login-done

--- lib/core/routing/router.dart
+++ lib/core/routing/router.dart
@@ -1,19 +1,16 @@
-
-
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:marketi/core/di/dependency_injection.dart';
 import 'package:marketi/features/auth/view/screen/login_screen.dart';
 import 'package:marketi/features/auth/view/screen/on_boarding.dart';
 import 'package:marketi/features/auth/view/screen/sign_up_screen.dart';
 import 'package:marketi/features/auth/view/screen/splash_screen.dart';
-
+import 'package:marketi/features/auth/view_model/cubit/login_cubit/login_cubit.dart';
 
 import 'core/constant/string.dart';
 
 class AppRouter {
-
-  AppRouter() {
-
-  }
+  AppRouter() {}
 
   Route? generateRoute(RouteSettings settings) {
     switch (settings.name) {
@@ -27,11 +24,12 @@ class AppRouter {
           builder: (_) => OnBoardingScreen(),
         );
       case login:
-        return MaterialPageRoute(builder: (_)=>LoginScreen());
+        return MaterialPageRoute(
+            builder: (_) => BlocProvider(
+                create: (BuildContext context) => getIt<LoginCubit>(),
+                child: const LoginScreen()));
       case signup:
-        return MaterialPageRoute(builder: (_)=>SignUp());
-
-
+        return MaterialPageRoute(builder: (_) => SignUp());
     }
   }
 }
